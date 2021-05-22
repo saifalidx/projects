3e682bde9e729e731416dabd0d0ea37d830fa840
saifalidx <61383026+saifalidx@users.noreply.github.com>
2021-05-22 22:57:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+<App />, document.getElementById('root')
+)
